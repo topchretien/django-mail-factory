cdc6c3ef376c4341563a3f147ca4ce96e403dcbc
florent.veillon <florent.veillon@nyleza.com>
2023-12-07 11:11:10 +0100

    feat(): Django 4.0+ use re_path

--- mail_factory/urls.py
+++ mail_factory/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """URLconf for mail_factory admin interface."""
-from django.conf.urls import url
+from django.urls import re_path
 
 from django.conf import settings
 
@@ -11,16 +11,16 @@ LANGUAGE_CODES = '|'.join([code for code, name in settings.LANGUAGES])
 
 
 urlpatterns = [
-    url(r'^$',
+    re_path(r'^$',
         mail_list,
         name='mail_factory_list'),
-    url(r'^detail/(?P<mail_name>.*)/$',
+    re_path(r'^detail/(?P<mail_name>.*)/$',
         form,
         name='mail_factory_form'),
-    url(r'^preview/(?P<lang>(%s))/(?P<mail_name>.*)/$' % LANGUAGE_CODES,
+    re_path(r'^preview/(?P<lang>(%s))/(?P<mail_name>.*)/$' % LANGUAGE_CODES,
         preview_message,
         name='mail_factory_preview_message'),
-    url(r'^html_not_found/(?P<mail_name>.*)/$',
+    re_path(r'^html_not_found/(?P<mail_name>.*)/$',
         html_not_found,
         name='mail_factory_html_not_found'),
 ]
